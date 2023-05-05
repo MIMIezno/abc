3aadb1904f0245753f6a5a360ee83e31eae95ec7
MIMIezno <ronnamiez2@gmail.com>
2023-05-05 12:14:01 +0800

    commit

--- app/src/main/java/com/example/workeatwork/Front.java
+++ app/src/main/java/com/example/workeatwork/Front.java
@@ -2,7 +2,9 @@ package com.example.workeatwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Front extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Front extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+    }
+//open register
+    public void openActivity(View view) {
+        Intent intent = new Intent(Front.this, Register.class);
+        startActivity(intent);
+    }
+
+    public void openLogin(View view) {
+        Intent intent = new Intent(Front.this, Login.class);
+        startActivity(intent);
     }
 }
