ad7a44f8dd523bb2f8ecdecace524ee127b6afb3
Niño Bacaoco <Nino.bacaoco@gmail.com>
2023-05-07 21:11:03 +0800

    homepage fck

--- app/src/main/java/com/example/workeatwork/Homepage.java
+++ app/src/main/java/com/example/workeatwork/Homepage.java
@@ -3,6 +3,9 @@ package com.example.workeatwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.widget.TextView;
 
 public class Homepage extends AppCompatActivity {
 
@@ -11,4 +14,10 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
+
+    TextView textView = findViewById(R.id.textView);
+    String text = "1,200\n";
+    SpannableString spannableString = new SpannableString(text);
+    spannableString.setSpan(new AbsoluteSizeSpan(24, true), 0, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    textView.setText(spannableString);
 }
