d28f8a63812eb13adca78a35e349a860daf53ab9
Niño Bacaoco <Nino.bacaoco@gmail.com>
2023-05-15 16:27:38 +0800

    clickable

--- app/src/main/java/com/example/workeatwork/More.java
+++ app/src/main/java/com/example/workeatwork/More.java
@@ -2,7 +2,9 @@ package com.example.workeatwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class More extends AppCompatActivity {
 
@@ -10,5 +12,40 @@ public class More extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more);
+    }
+
+    public void openAccount(View view) {
+        Intent intent = new Intent(More.this, Account.class);
+        startActivity(intent);
+    }
+
+    public void openRecipes(View view) {
+        Intent intent = new Intent(More.this, Recipes.class);
+        startActivity(intent);
+    }
+
+    public void openHistory(View view) {
+        Intent intent = new Intent(More.this, History.class);
+        startActivity(intent);
+    }
+
+    public void openReminder(View view) {
+        Intent intent = new Intent(More.this, Reminder.class);
+        startActivity(intent);
+    }
+
+    public void openWorkout(View view) {
+        Intent intent = new Intent(More.this, Workout.class);
+        startActivity(intent);
+    }
+
+    public void openSettings(View view) {
+        Intent intent = new Intent(More.this, Settings.class);
+        startActivity(intent);
+    }
+
+    public void openLogin(View view) {
+        Intent intent = new Intent(More.this, Login.class);
+        startActivity(intent);
     }
 }
