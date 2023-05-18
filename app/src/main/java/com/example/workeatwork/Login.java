c346818136048424b970c0519c25ae316621545e
Niño Bacaoco <Nino.bacaoco@gmail.com>
2023-05-18 23:26:09 +0800

    login if else

--- app/src/main/java/com/example/workeatwork/Login.java
+++ app/src/main/java/com/example/workeatwork/Login.java
@@ -5,17 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
+    private TextInputEditText emailEditText;
+    private TextInputEditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
     }
 
     public void openContinue(View view) {
-        Intent intent = new Intent(Login.this, Continue.class);
-        startActivity(intent);
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // Perform login validation
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+        } else if (email.equals("sample@gmail.com") && password.equals("123")) {
+            // Login successful
+            Intent intent = new Intent(this, Continue.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // Login failed
+            Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
