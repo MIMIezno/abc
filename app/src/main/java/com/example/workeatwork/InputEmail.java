892e8f22e0368b0f267a33e2da465ed418e408c4
MIMIezno <ronnamiez2@gmail.com>
2023-05-21 17:13:32 +0800

    REGISTER , LOG IN

--- app/src/main/java/com/example/workeatwork/InputEmail.java
+++ app/src/main/java/com/example/workeatwork/InputEmail.java
@@ -1,21 +1,91 @@
 package com.example.workeatwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class InputEmail extends AppCompatActivity {
+
+    private TextInputEditText emailEditText, passwordEditTex;
+
+    ImageView Register;
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_email);
-    }
 
+
+        emailEditText = findViewById(R.id.email);
+        passwordEditTex = findViewById(R.id.password);
+        Register = findViewById(R.id.Register);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email, password;
+                email = String.valueOf(emailEditText.getText());
+                password = String.valueOf(passwordEditTex.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(InputEmail.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(InputEmail.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(InputEmail.this, "Registration Successfully", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(InputEmail.this, Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                        else{
+                                        Toast.makeText(InputEmail.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+
+            }
+        });
+
+
+
+    }
+}
+
+
+
+
+
+
+
+/*
     public void openHomepage(View view) {
         Intent intent = new Intent(InputEmail.this, Homepage.class);
         startActivity(intent);
     }
-}
+}*/
--- app/src/main/java/com/example/workeatwork/Login.java
+++ app/src/main/java/com/example/workeatwork/Login.java
@@ -1,27 +1,81 @@
 package com.example.workeatwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    private TextInputEditText emailEditText;
-    private TextInputEditText passwordEditText;
+    private TextInputEditText emailEditText, passwordEditTex;
+
+    Button login;
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        emailEditText = findViewById(R.id.email);
-        passwordEditText = findViewById(R.id.password);
-    }
 
+        emailEditText = findViewById(R.id.email);
+        passwordEditTex = findViewById(R.id.password);
+        login = findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = String.valueOf(emailEditText.getText());
+                password = String.valueOf(passwordEditTex.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Login.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Login.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                firebaseAuth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Login.this, Homepage.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Account Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+     /*   emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);*/
+    }
+}
+
+
+
+
+
+/*
     public void openHomepage(View view) {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
@@ -39,4 +93,4 @@ public class Login extends AppCompatActivity {
             Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show();
         }
     }
-}
+}*/
