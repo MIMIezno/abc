6705f4b26355cb423f4a7d961e631adb23171a4e
Niño Bacaoco <Nino.bacaoco@gmail.com>
2023-05-27 01:42:35 +0800

    Recipes

--- app/src/main/java/com/example/workeatwork/Recipes.java
+++ app/src/main/java/com/example/workeatwork/Recipes.java
@@ -1,6 +1,9 @@
 package com.example.workeatwork;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,10 @@ public class Recipes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+    }
+
+    public void openSaveFood(View view) {
+        Intent intent = new Intent(Recipes.this, Food.class);
+        startActivity(intent);
     }
 }
